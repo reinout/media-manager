3b8eb836f236498969c367b4012e7b9a650c6686
Reinout van Rees <reinout@vanrees.org>
2013-02-05 14:03:28 +0100

    Added git annex init to a separate testcase

--- media_manager/tests/test_sourcerepo.py
+++ media_manager/tests/test_sourcerepo.py
@@ -10,6 +10,21 @@ def exists(*path_elements):
     return os.path.exists(os.path.join(*path_elements))
 
 
+class ChdirContext(object):
+    """Context manager for a 'with' loop."""
+    def __init__(self, target):
+        self._original = os.getcwd()
+        self._target = target
+
+    def __enter__(self):
+        os.chdir(self._target)
+        return self
+
+    def __exit__(self, *args):
+        os.chdir(self._original)
+        return self
+
+
 class SourceRepoTest(unittest.TestCase):
 
     def setUp(self):
@@ -35,3 +50,13 @@ class SourceRepoTest(unittest.TestCase):
         self.assertRaises(AssertionError,
                           self.source_repo.ensure_directory,
                           'something_else')
+
+
+class SourceRepoWithAnnexTest(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        with ChdirContext(self.tempdir):
+            os.system("git init")
+            os.system("git annex init tempdir")
+        self.source_repo = sourcerepo.SourceRepo(self.tempdir)
